3526b06bb787026eb88bda9f3b2a55c70af8aee6
Jose Antonio <joseantoniojuan@facephi.com>
2022-02-09 13:38:21 +0000

    Upload new ios version: 1.0.0

---
+++ flutter/agora_rtc_engine.xcframework/ios-arm64_armv7/agora_rtc_engine.framework/Headers/agora_rtc_engine-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AgoraRtcEnginePlugin.h"
+#import "AgoraRtcEngineKit.h"
+#import "RtcEnginePlugin.h"
+
+FOUNDATION_EXPORT double agora_rtc_engineVersionNumber;
+FOUNDATION_EXPORT const unsigned char agora_rtc_engineVersionString[];
+
