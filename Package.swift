f15e52a0bbb50c2302aebc99d37f8f7878d320ba
Facephi <developer@facephi.com>
2022-04-25 08:28:56 +0000

    Upload new ios version: 1.0.3

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         .binaryTarget(name: "App", path: "Frameworks/App.xcframework"),
 
         .binaryTarget(name: "device_info_plus", path: "Frameworks/device_info_plus.xcframework"),
-        //.binaryTarget(name: "Flutter", path: "Frameworks/Flutter.xcframework"),
-        .binaryTarget(name: "Flutter",url:  "https://dev-ctlzip-s3.s3.eu-west-1.amazonaws.com/sdk-flutter/classic/iOS/1.0.3/Flutter.xcframework.zip",checksum: "ce50a6687395c5235d2102012721799439bfd56749d8e3c4827b4423ef842e70"),
+        .binaryTarget(name: "Flutter", path: "Frameworks/Flutter.xcframework"),
+    /*    .binaryTarget(name: "Flutter",url:  "https://dev-ctlzip-s3.s3.eu-west-1.amazonaws.com/sdk-flutter/ios+/Flutter.xcframework.zip",checksum: "4a946dfd90ffcf54d1400999828293eeab78c372858ee3e3416a6db4c1666170"),*/
         .binaryTarget(name: "FlutterPluginRegistrant", path: "Frameworks/FlutterPluginRegistrant.xcframework"),
         .binaryTarget(name: "selphid_plugin", path: "Frameworks/selphid_plugin.xcframework"),
         .binaryTarget(name: "selphi_face_plugin", path: "Frameworks/selphi_face_plugin.xcframework"),
@@ -45,4 +45,3 @@ let package = Package(
         .binaryTarget(name: "FPBExtractoriOS", path: "FrameworksSelphi/FPBExtractoriOS.xcframework"),
     ]
 )
-
